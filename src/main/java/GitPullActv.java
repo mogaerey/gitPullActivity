b222f289f4ec4225d3079077a8010bd082c61f5d
Cecilia Ames <afleury@argyletech.net>
2019-07-22 10:03:12 -0500

    Adding new changes

--- src/main/java/GitPullActv.java
+++ src/main/java/GitPullActv.java
@@ -2,5 +2,6 @@ public class GitPullActv {
     public static void main(String[] args) {
         System.out.println("Hi Alison!");
         System.out.println("Hi Grace!");
+        System.out.println("Hi Monique!");
     }
 }
